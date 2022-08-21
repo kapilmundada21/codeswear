1e915d00a00e4796339e3e9e7774f22c093a7cde
kapilmundada <kapilmundada21@gmail.com>
2022-08-21 17:45:24 +0530

    mongo error

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -6,9 +6,7 @@ const connectDb = handler => async (req, res) => {
     }
     await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
+        useUnifiedTopology: true
     })
     return handler(req, res);
 }
